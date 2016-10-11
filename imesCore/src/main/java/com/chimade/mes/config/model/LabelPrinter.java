051f8196483f29d3b846e0be64fd687cad035465
tommy1860 <yinrongwang@163.com>
2016-10-11 14:19:21 +0800

    Add name

--- imesCore/src/main/java/com/chimade/mes/config/model/LabelPrinter.java
+++ imesCore/src/main/java/com/chimade/mes/config/model/LabelPrinter.java
@@ -10,6 +10,7 @@ package com.chimade.mes.config.model;
 public class LabelPrinter {
 	private int id;
 	private String code;
+	private String name;
 	private String host;
 	private String port;
 	private String description;
